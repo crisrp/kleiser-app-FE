dfea9287cc97d22e09847f0893fc91da4d8476af
Cristina Ruiz <cruiz731@gmail.com>
2016-07-23 20:22:17 -0400

    can make calls to mongodb for all phrases

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
-// on click button display the definition
-var clickDefine = document.getElementById('wordset1-define')
-var definitionFirstWord = document.getElementById('p1d1');
-var definitionSecondWord = document.getElementById('p1d2');
-clickDefine.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('clicked');
-    console.log(event);
-    definitionFirstWord.removeAttribute('class','hidden');
-    definitionSecondWord.removeAttribute('class','hidden');
+document.addEventListener('DOMContentLoaded', function(){
+
+
+  console.log('kleiser app js FE file loaded...');
+
+
+//get elements
+
+
+var gobutton = document.getElementById('show-phrases');
+
+var url = 'http://localhost:3000';
+
+gobutton.addEventListener('click', function (){
+  $.ajax({
+    url: url + '/usefulphrases',
+    dataType: 'json'
+  }).done(function(response) {
+    document.createElement('p')
+    console.log(response);
+  });
+});
+
+/************************************
+get random id for phrase collection
+Generate a random number between 0 and limit
+presently 100 useful phrases available
+**************************************/
+
+function randomRecord(documentCount, numResults) {
+  //random number between 0 - 64
+  var recIdArr = [];
+  for (var i = 0; i < numResults; i++){
+    var recId = Math.floor(Math.random() * (documentCount - 0));
+    recIdArr.push(recId);
+  }
+  console.log(recIdArr);
+  return recIdArr;
+};
+
+//   console.log(clickDefine[0]);
+// var definitionFirstWord = document.getElementById('p1d1');
+// var definitionSecondWord = document.getElementById('p1d2');
+// clickDefine.addEventListener(
 });
